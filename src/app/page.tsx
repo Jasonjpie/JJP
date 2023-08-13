34be841a0c093a6d0e56e42f613e2f8510b90927
HailemichaelSintayehu <hmichaelsintayehu@gmail.com>
2023-08-13 09:53:45 +0300

    implement projects section UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import FeaturedProperties from '@/components/homepage/FeaturedProperties'
 import HeroSection from '@/components/homepage/HeroSection'
 import Footer from '@/components/common/Footer'
 import Benefits from '@/components/homepage/Benefits'
+import FeaturedProjects from '@/components/homepage/FeaturedProjects'
+import Projects from '@/components/homepage/Projects'
 
 export default function Home() {
   return (
@@ -11,7 +13,9 @@ export default function Home() {
       <NavBar />
       <HeroSection />
       <FeaturedProperties />
+      <FeaturedProjects />
       <Benefits/>
+      <Projects />
       <Footer />
     </div>
   )
---
+++ src/components/homepage/Projects.tsx
@@ -0,0 +1,32 @@
+import { projects } from "@/data"
+import Image from "next/image"
+
+type Props = {}
+
+const Projects = (props: Props) => {
+  return (
+    <div className="w-full flex flex-col gap-5 my-44">
+        <div className="font-bold text-5xl text-center">OUR PROJECTS</div>
+        <div className="text-center max-w-lg mx-auto">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</div>
+        <div className="grid grid-cols-3 gap-5">
+            {
+                projects.map((project, index) => (
+           <div key={index} className="relative rounded-md">
+             <Image className="p-0 m-0" src={`/images/projects/${project.image}`} width={600} height={900} alt=''/>
+             {/* <div className="absolute bottom-24 left-10 text-5xl max-w-[500px] font-bold text-white leading-relaxed">{project.name}</div> */}
+             {/* <div className="absolute bottom-12 left-10 text-white text-2xl">View Project</div> */}
+            </div>
+                ))
+            }
+            
+            
+        </div>
+        <button className="py-3 px-12 bg-gradient-to-br w-fit mx-auto mt-5 from-[#696CE6] to-[#1C1F9F] text-white">
+          Discover More
+        </button>
+        
+    </div>
+  )
+}
+
+export default Projects
