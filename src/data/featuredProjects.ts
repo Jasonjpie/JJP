57d659651f5fc32ee8c5b9ee591c7be608b9f9e3
HailemichaelSintayehu <hmichaelsintayehu@gmail.com>
2023-08-27 22:05:43 +0300

    name and url of the featured projects

---
+++ src/data/featuredProjects.ts
@@ -0,0 +1,311 @@
+export const diningRoom = [
+    {
+     
+      name: "embury",
+      url: "https://res.cloudinary.com/desqmglcs/image/upload/v1692282376/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/1038%20%20Embury/5_vpxwun.jpg",
+    },
+    {
+      name:"",
+      url:""
+    }
+  ];
+
+  export const bathroom = [
+    {
+      name: "embury",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692282375/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/1038%20%20Embury/7_pl7znr.jpg"
+    },
+    {
+      name:"wood bridge",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692280586/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/12005%20Woodbridge/SKY_PHOTOGRAPHY_LA-29_xfhez4.jpg"
+    },
+    {
+      name:"wood bridge",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692280583/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/12005%20Woodbridge/SKY_PHOTOGRAPHY_LA-41_v0msyz.jpg"
+    },
+    {
+      name:"wood bridge",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692280566/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/12005%20Woodbridge/SKY_PHOTOGRAPHY_LA-39_aesby3.jpg"
+    },
+    {
+      name:"wood bridge",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692280554/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/12005%20Woodbridge/SKY_PHOTOGRAPHY_LA-35_xhbx6m.jpg"
+    },
+    {
+      name:"hortense",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692281925/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/12721%20Hortense/JD2_4398-Edit_lmjamz.jpg"
+    },
+    {
+      name:"hortense",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692281911/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/12721%20Hortense/JD2_4461-Edit_pkugjf.jpg"
+    },
+    {
+      name:"rancho st",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692282366/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/17320%20Rancho%20st/17320Rancho_HiRes-46-of-651-1024x683_h5seiv.jpg"
+    }
+    
+  ];
+  export const livingRoom = [
+    {
+      name: "embury",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692282375/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/1038%20%20Embury/2_eb1vkv.jpg"
+    },
+    {
+      name:"wood bridge",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692280587/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/12005%20Woodbridge/SKY_PHOTOGRAPHY_LA-48_zdqf5p.jpg"
+    },
+    {
+      name:"wood bridge",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692280584/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/12005%20Woodbridge/SKY_PHOTOGRAPHY_LA-24_x02ahg.jpg"
+    },
+    {
+      name:"wood bridge",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692280583/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/12005%20Woodbridge/SKY_PHOTOGRAPHY_LA-25_tvyqgz.jpg"
+    },
+    {
+      name:"wood bridge",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692280582/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/12005%20Woodbridge/SKY_PHOTOGRAPHY_LA-20_zyugbh.jpg"
+    },
+    {
+      name:"wood bridge",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692280577/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/12005%20Woodbridge/SKY_PHOTOGRAPHY_LA-14_ax0k8g.jpg"  
+    },
+    {
+      name:"wood bridge",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692280577/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/12005%20Woodbridge/SKY_PHOTOGRAPHY_LA-22_tgjts7.jpg"
+    },
+    {
+      name:"wood bridge",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692280576/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/12005%20Woodbridge/SKY_PHOTOGRAPHY_LA-12_txk8pe.jpg"
+    },
+    {
+      name:"hortense",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692281893/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/12721%20Hortense/JD2_4579_snmmmj.jpg"
+    },
+    {
+      name:"shankola",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692280626/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/4405%20Sancola/4405SancolaAve_TolucaLake_CA91602_USAInteriors-183_c5x2da.jpg"
+    }
+  ];
+
+  export const bedroom = [
+    {
+      name: "wood bridge",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692280586/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/12005%20Woodbridge/SKY_PHOTOGRAPHY_LA-31_vzvvwd.jpg"
+    },
+    {
+      name:"wood bridge",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692280557/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/12005%20Woodbridge/SKY_PHOTOGRAPHY_LA-32_ydpjtj.jpg"
+    },
+    {
+      name:"wood bridge",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692280557/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/12005%20Woodbridge/SKY_PHOTOGRAPHY_LA-38_d3ntul.jpg"
+    },
+    {
+      name:"hortense",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692282121/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/12721%20Hortense/JD2_4425-Edit_y3eisg.jpg"
+    },
+    {
+      name:"valley vista",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692282338/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/15125%20Valley%20Vista/15125_Valley_Vista_Boulevard_-_High_Resolution_21_f8jb6b.jpg"
+    },
+    {
+      name:"rancho st",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692282374/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/17320%20Rancho%20st/17320Rancho_HiRes-48-of-651-1024x683_qwa2jy.jpg"
+    },
+    {
+      name:"rancho st",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692282367/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/17320%20Rancho%20st/17320Rancho_HiRes-50-of-651-1024x683_i0ttlr.jpg"
+    }
+  ]
+
+  export const stairs = [
+    {
+      name:"wood bridge",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692280584/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/12005%20Woodbridge/SKY_PHOTOGRAPHY_LA-19_swcid1.jpg"
+    },
+    {
+      name:"wood bridge",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692280577/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/12005%20Woodbridge/SKY_PHOTOGRAPHY_LA-30_anlbf8.jpg"
+    },
+    {
+      name:"wood bridge",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692279924/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/12005%20Woodbridge/SKY_PHOTOGRAPHY_LA-30_ms78oj.jpg"
+    },
+    {
+      name:"hortense",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692281920/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/12721%20Hortense/JD2_4572_vyncea.jpg"
+    },
+    {
+      name:"valley vista",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692282358/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/15125%20Valley%20Vista/15125_Valley_Vista_Boulevard_-_High_Resolution_20_kauu10.jpg"
+    },
+    {
+      name:"valley vista",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692282337/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/15125%20Valley%20Vista/15125_Valley_Vista_Boulevard_-_High_Resolution_19_ezblpj.jpg"
+    },
+    {
+      name:"rancho st",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692282367/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/17320%20Rancho%20st/17320Rancho_HiRes-32-of-651-1024x683_nri1gk.jpg"
+    },
+    {
+      name:"beck",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692280549/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/4054%20Beck/4054Beck_HiRes_26_of_50_kzsdgl.jpg"
+    },
+    {
+      name:"shadyglade",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692282381/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/4322%20shadyglade/SkyFilm-21-1024x684_atuzhx.jpg"
+    },
+    {
+      name:"lliff",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692281552/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/770%20Iliff%20%282018%29/SkyFilm_58_y2fa0f.jpg"
+    },
+    {
+      name:"lliff",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692281543/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/770%20Iliff%20%282018%29/SkyFilm_54_fadraf.jpg"
+    },
+    {
+      name:"lliff",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692281536/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/770%20Iliff%20%282018%29/SkyFilm_49_x9kdxk.jpg"
+    }
+  ]
+
+  export const kitchen  = [
+    {
+      name:"hortense",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692281923/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/12721%20Hortense/JD2_4465-Edit_df7opy.jpg"
+    },
+    {
+      name:"beck",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692280545/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/4054%20Beck/4054Beck_HiRes_17_of_50_cdh1qa.jpg"
+    },
+    {
+      name:"beck",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692280528/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/4054%20Beck/4054Beck_HiRes_16_of_50_fylbke.jpg"
+    },
+    {
+      name:"sancola",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692280628/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/4405%20Sancola/4405SancolaAve_TolucaLake_CA91602_USAInteriors-62_mhxkks.jpg"
+    },
+    {
+      name:"lliff",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692281549/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/770%20Iliff%20%282018%29/SkyFilm_18_rsev7e.jpg"
+    }
+  ]
+
+  export const wineCabinet = [
+    {
+      name:"wood bridge",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692280572/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/12005%20Woodbridge/SKY_PHOTOGRAPHY_LA-27_z1quft.jpg"
+    },
+    {
+      name:"hortense",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692282123/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/12721%20Hortense/JD2_4580_clnc28.jpg"
+    },
+    {
+      name:"hortense",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692280054/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/12721%20Hortense/JD2_4495_szep1c.jpg"
+    },
+    {
+      name:"valley vista",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692282355/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/15125%20Valley%20Vista/15125_Valley_Vista_Boulevard_-_High_Resolution_61_cprd5p.jpg"
+    },
+    {
+      name:"beck",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692280550/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/4054%20Beck/4054Beck_HiRes_11_of_50_cerrqe.jpg"
+    },
+    {
+      name:"beck",
+      url:"https://res.cloudinary.com/https://res.cloudinary.com/desqmglcs/image/upload/v1692280503/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/4054%20Beck/4054Beck_HiRes_10_of_50_tnchl8.jpgdesqmglcs/image/upload/v1692280503/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/4054%20Beck/4054Beck_HiRes_10_of_50_tnchl8.jpg"
+    }
+  ]
+
+  export const pool  = [
+    {
+      name:"wood bridge",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692279936/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/12005%20Woodbridge/SKY_PHOTOGRAPHY_LA-5_orwdko.jpg"
+    },
+    {
+      name:"wood bridge",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692279908/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/12005%20Woodbridge/SKY_PHOTOGRAPHY_LA-8_hakak4.jpg"
+    },
+    {
+      name:"hortense",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692282125/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/12721%20Hortense/JD2_4570_zocmfh.jpg"
+    },
+    {
+      name:"hortense",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692281924/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/12721%20Hortense/JD2_4522-Edit_lnl7ys.jpg"
+    },
+    {
+      name:"hortense",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692281896/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/12721%20Hortense/JD2_4561-Edit_zztnuz.jpg"
+    },
+    {
+      name:"valley vista",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692282371/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/15125%20Valley%20Vista/15125_Valley_Vista_Boulevard_-_High_Resolution_53_gtvwf3.jpg"
+    },
+    {
+      name:"valley vista",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692282356/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/15125%20Valley%20Vista/15125_Valley_Vista_Boulevard_-_High_Resolution_67_w1exwo.jpg"
+    },
+    {
+      name:"rancho st",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692282372/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/17320%20Rancho%20st/3-1024x683_xnryje.jpg"
+    },
+    {
+      name:"beck",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692280513/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/4054%20Beck/4054Beck_HiRes_44_of_50_cqxcgk.jpg"
+    },
+    {
+      name:"shadyglade",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692282383/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/4322%20shadyglade/SkyFilm-70-1024x689_bsxugr.jpg"
+    },
+    {
+      name:"lliff",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692281535/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/770%20Iliff%20%282018%29/SkyFilm_63_gvuvbb.jpg"
+    }
+
+  ]
+  export const filmTheature  = [
+    {
+      name:"rancho st",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692282370/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/17320%20Rancho%20st/15-1024x683_c1teom.jpg"
+    },
+    {
+      name:"beck",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692280549/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/4054%20Beck/4054Beck_HiRes_24_of_50_wlji6z.jpg"
+    },
+    {
+      name:"sancola",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692280598/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/4405%20Sancola/4405SancolaAve_TolucaLake_CA91602_USAInteriors-140_ps3j4f.jpg"
+    },
+    {
+      name:"lliff",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692281553/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/770%20Iliff%20%282018%29/SkyFilm_56_sarzgv.jpg"
+    }
+  ]
+  export const fire  = [
+    {
+      name:"beck",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692280552/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/4054%20Beck/4054Beck_HiRes_23_of_50_ho2u5g.jpg"
+    },
+    {
+      name:"beck",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692280516/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/4054%20Beck/4054Beck_HiRes_6_of_50_v2kt2q.jpg"
+    }
+  ]
+
+  export const houseGym  = [
+    {
+      name:"sancola",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692280610/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/4405%20Sancola/4405SancolaAve_TolucaLake_CA91602_USAInteriors-159_ftapua.jpg"
+    }
+  ]
+
+  export const homeMassage  = [
+    {
+      name:"sancola",
+      url:"https://res.cloudinary.com/desqmglcs/image/upload/v1692280601/House%20Pictures-20230812T204648Z-002%20%28copy%29/House%20Pictures/4405%20Sancola/4405SancolaAve_TolucaLake_CA91602_USAInteriors-143_fkwsi7.jpg"
+    }
+  ]
