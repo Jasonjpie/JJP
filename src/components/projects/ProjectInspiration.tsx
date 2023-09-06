ce8ac292dde327143c1d7d5770e6ed542c45f47b
HailemichaelSintayehu <hmichaelsintayehu@gmail.com>
2023-09-06 18:45:23 +0300

    implement project gallery page UI

--- src/components/projects/ProjectInspiration.tsx
+++ src/components/projects/ProjectInspiration.tsx
@@ -6,12 +6,12 @@ type Props = {}
 const ProjectInspiration = (props: Props) => {
   return (
     <div className="flex flex-wrap lg:flex-nowrap lg:justify-end gap-5 items-center w-full h-fit bg-[#F3F4FC] p-10 pr-0">
-        <div className='p-10 max-w-[550px] space-y-5 h-fit'>
-            <div className="font-medium font-poppins text-5xl">50+ Beautiful rooms & inspirations</div>
+        <div className='p-10 max-w-[550px] w-full space-y-5 h-fit'>
+            {/* <div className="font-medium font-poppins text-5xl">50+ Beautiful rooms & inspirations</div>
             <div className="text-[#616161]">Our designer already made a lot of beautiful prototipe of rooms that inspire you</div>
             <button className="text-white bg-black px-8 py-2">
                 Explore More
-            </button>
+            </button> */}
         </div>
         <div className="w-full flex justify-between gap-5">
         <div className="relative w-[60%] h-[350px] md:h-[450px] lg:h-[550px]">
