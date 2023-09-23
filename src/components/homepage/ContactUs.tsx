be6a4de00fc2d71ad167de0a905d85848621cc60
saint <hmichaelsintayehu@gmail.com>
2023-09-23 14:09:53 +0300

    added send contact form

--- src/components/homepage/ContactUs.tsx
+++ src/components/homepage/ContactUs.tsx
@@ -1,47 +1,141 @@
-import Image from "next/image"
-import { useState } from "react"
-import Container from "../common/Container"
-type Props = {}
+import Image from "next/image";
+import { useState } from "react";
+import emailjs from "emailjs-com"; // Import Email.js
+
+type Props = {};
 
 const ContactUs = (props: Props) => {
-    const [ name, setName ] = useState('')
-    const [ email, setEmail ] = useState('')
-    const [ subject, setSubject ] = useState('')
-    const [ message, setMessage ] = useState('')
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [subject, setSubject] = useState("");
+    const [message, setMessage] = useState("");
+    const [successMessage, setSuccessMessage] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+    const [isSubmitting, setIsSubmitting] = useState(false); // Add loading state
 
-    const SERVICE_ID = "service_7off9vs";
-    const TEMPLATE_ID = "template_nt2q4ra";
-    const PUBLIC_KEY = "PJrVfioBoNH7BNp0P";
+    const serviceID = "service_e3gfj5u"; // Replace with your Email.js service ID
+    const userID = "lBEGkhwko5Z_AnqC5"; // Replace with your Email.js user ID
+    const templateID = "template_lsf6ept"; // Replace with your Email.js template ID
+    emailjs.init(userID); // Initialize Email.js with your user ID
 
-    // const handleSubmit = (e) => {
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
 
-    // }
+        // Set loading state to true while submitting
+        setIsSubmitting(true);
 
-  return (
- <div className="w-full flex columns-2  py-24">
-        <div className="flex flex-col gap-5 lg:w-[60%] p-10">
-            <div className="text-5xl lg:text-6xl font-bold font-poppins">Contact Us</div>
-            <div className="leading-loose font-poppins ">
-                We would be honored to help you while you make changes to your home. Get in touch with us right now to start realizing your vision.
-            </div>
-            <form className="flex flex-col gap-7">
-                <div className="w-full flex flex-wrap gap-5">
-                    <input value={name} onChange={(e) => setName(e.target.value)} type="text" placeholder="Your Name" className="flex-1 p-2.5 border" />
-                    <input value={email} onChange={(e) => setEmail(e.target.value)} type="text" placeholder="Your Email" className="flex-1 p-2.5 border" />
+        // Use Email.js to send the email
+        emailjs
+            .send(serviceID, templateID, {
+                to_name: "Jeson P.", // Replace with recipient email
+                from_name: email,
+                from_email: email,
+                subject: subject,
+                message: message,
+                reply_to: email,
+            })
+            .then(
+                (response) => {
+                    console.log("Email sent:", response);
+                    setSuccessMessage("Email sent successfully!");
+
+                    // Clear form fields upon successful submission
+                    setName("");
+                    setEmail("");
+                    setSubject("");
+                    setMessage("");
+
+                    // Clear success message after 5 seconds (5000 milliseconds)
+                    setTimeout(() => {
+                        setSuccessMessage("");
+                    }, 5000);
+                },
+                (error) => {
+                    console.error("Error sending email:", error);
+                    setErrorMessage("Failed to send email. Please try again later.");
+
+                    // Clear error message after 5 seconds (5000 milliseconds)
+                    setTimeout(() => {
+                        setErrorMessage("");
+                    }, 5000);
+                }
+            )
+            .finally(() => {
+                // Set loading state to false after submission (whether successful or not)
+                setIsSubmitting(false);
+            });
+    };
+
+    return (
+        <div className="w-full flex columns-2  py-24">
+            <div className="flex flex-col gap-5 lg:w-[60%] p-10">
+                <div className="text-5xl lg:text-6xl font-bold font-poppins">
+                    Contact Us
                 </div>
-                <input value={subject} onChange={(e) => setSubject(e.target.value)} type="text" placeholder="Subject" className="flex-1 p-2.5 border" />
-                <textarea value={message} onChange={(e) => setMessage(e.target.value)} name="" id="" className="px-5 py-3 resize-none" placeholder="Message" cols={30} rows={2}></textarea>
-                <button type="submit" className="bg-black px-8 py-3 mt-10 self-start text-white">
-                    Submit
-                </button>
-            </form>
+                <div className="leading-loose font-poppins ">
+                    We would be honored to help you while you make changes to your home.
+                    Get in touch with us right now to start realizing your vision.
+                </div>
+                {successMessage && (
+                    <div className="text-green-500">{successMessage}</div>
+                )}
+                {errorMessage && <div className="text-red-500">{errorMessage}</div>}
+                <form className="flex flex-col gap-7" onSubmit={handleSubmit}>
+                    <div className="w-full flex flex-wrap gap-5">
+                        <input
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            type="text"
+                            placeholder="Your Name"
+                            className="flex-1 p-2.5 border"
+                        />
+                        <input
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            type="text"
+                            placeholder="Your Email"
+                            className="flex-1 p-2.5 border"
+                        />
+                    </div>
+                    <input
+                        value={subject}
+                        onChange={(e) => setSubject(e.target.value)}
+                        type="text"
+                        placeholder="Subject"
+                        className="flex-1 p-2.5 border"
+                    />
+                    <textarea
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                        name=""
+                        id=""
+                        className="px-5 py-3 resize-none"
+                        placeholder="Message"
+                        cols={30}
+                        rows={2}
+                    ></textarea>
+                    <button
+                        type="submit"
+                        className={`bg-black px-8 py-3 mt-10 self-start text-white ${isSubmitting ? "opacity-50 cursor-wait" : ""
+                            }`}
+                        disabled={isSubmitting}
+                    >
+                        {isSubmitting ? "Sending..." : "Submit"}
+                    </button>
+                </form>
+            </div>
+            <div className="relative lg:block hidden w-[40%]">
+                <Image src="/Icons/world.svg" fill alt="World Map" />
+                <Image
+                    className="absolute top-64 left-32"
+                    src="/Icons/location-pointer.svg"
+                    width={50}
+                    height={50}
+                    alt="World Map"
+                />
+            </div>
         </div>
-        <div className="relative lg:block hidden w-[40%]">
-            <Image src='/Icons/world.svg' fill alt="World Map"/>
-            <Image className="absolute top-64 left-32" src='/Icons/location-pointer.svg' width={50} height={50} alt="World Map"/>
-        </div>
-    </div>   
-  )
-}
+    );
+};
 
-export default ContactUs
+export default ContactUs;
